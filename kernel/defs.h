dedb131924d5a3d2169cab7db904de89f7ed9395
Vitalis <Vitalis@foxmail.com>
2024-09-06 15:21:26 +0800

    Finish Lab06 : COW

--- kernel/defs.h
+++ kernel/defs.h
@@ -145,6 +145,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+int             solve_cow_page(pagetable_t, uint64); // Add for Lab COW
 
 // uart.c
 void            uartinit(void);
@@ -170,6 +171,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+pte_t *         walk(pagetable_t pagetable, uint64 va, int alloc); // Add for Lab COW
+int             ref_add(uint64 pa, int n); // Add for Lab COW
 
 // plic.c
 void            plicinit(void);
