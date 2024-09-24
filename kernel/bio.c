da050d6f90d91518817b84f607ffc228e5ef6de7
Vitalis <Vitalis@foxmail.com>
2024-09-24 15:42:51 +0800

    Fixed a bug with [Special Case].

--- kernel/bio.c
+++ kernel/bio.c
@@ -45,6 +45,7 @@ void binit(void)
     initlock(&bcache.lock[i], lockname);
   }
 
+  // 这里让每个哈希桶都有相等数量的buffer cache，后续在bget()中可能会变动
   // 头插法，把所有 buf 插入到链表当中
   // Create linked list of buffers
   for (int i = 0; i < 13; i++)
@@ -100,16 +101,32 @@ bget(uint dev, uint blockno)
     }
   }
 
+  // 不应该在这里释放bcache.lock[bno]，因为下面还会用到的（下面的情况为特例，需要释放）
+
+  // 【特例】：假设当前进程为P1，已经获取了bcache.global_lock，那么为了避免在
+  // if(holding(&bcache.lock[i])) continue;
+  // else acquire(&bcache.lock[i]);
+  // if和else 中间 有另外一个进程P2 进入了bget()而获取到对应的bcache.lock[i]，他也在上面没有找到可用的buffer cache，也进入到下面，就会造成死锁
+  // 所以就需要先释放掉它，让原本的进程先处理
   
-  release(&bcache.lock[bno]);
+  if(holding(&bcache.global_lock))
+    release(&bcache.lock[bno]);
+  
+  // global 有什么用?
+  // 保证同一时刻，只能有一个进程进入下面的区域
+  // 否则如果有多个进程同时进入下面的区域，极端情况下所有进程都在下面，那么所有进程都不会acquire成功，造成panic
   acquire(&bcache.global_lock);
+
+  // 在【特例】情况下，P1完成后，P2进来就需要重新获取锁
+  if(!holding(&bcache.lock[bno]))
+    acquire(&bcache.lock[bno]);
 
   for (int i = 0; i < 13; i++)
   {
     if (i == bno) continue;
 
     if(holding(&bcache.lock[i])) continue;
-    acquire(&bcache.lock[i]);
+    else acquire(&bcache.lock[i]);
 
     for (b = bcache.head[i].prev; b != &bcache.head[i]; b = b->prev)
     {
@@ -123,6 +140,7 @@ bget(uint dev, uint blockno)
         b->prev->next = b->next;
         b->next->prev = b->prev;
         
+        // 把b对应的块，插回到[bno]中去
         b->prev = &bcache.head[bno];
         b->next = bcache.head[bno].next;
 
@@ -130,6 +148,7 @@ bget(uint dev, uint blockno)
         bcache.head[bno].next = b;
 
         release(&bcache.lock[i]);
+        release(&bcache.lock[bno]);
         release(&bcache.global_lock);
         acquiresleep(&b->lock);
 
@@ -137,9 +156,11 @@ bget(uint dev, uint blockno)
       }
     }
 
+    // 别忘了没找到的时候需要释放
     release(&bcache.lock[i]);
   }
 
+  release(&bcache.lock[bno]);
   release(&bcache.global_lock);
 
   panic("bget: no buffers");
